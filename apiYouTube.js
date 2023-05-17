4aae1e6dc8a7f2f3f604966a86845eb7fd80d8ae
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-17 15:49:09 -0500

    ya funciona falta solo el carusel

--- apiYouTube.js
+++ apiYouTube.js
@@ -1,7 +1,4 @@
-import  comentarios  from "./comentarios.js";
-import  videoRelacionados  from "./videosRelacionados.js";
-
-
+//autor:Wilfer Olrando Acosta Rodriguez
 async function buscar() {
     const buscar = document.getElementById("buscar").value;
     const url = `https://youtube138.p.rapidapi.com/search/?q=${buscar}&hl=en&gl=US`;
@@ -23,6 +20,53 @@ try {
     comentarios(id);
 	videoRelacionados(id);
 	document.getElementById("description").innerHTML = `<br><h2>Comentarios</h2><br><p></p>`;
+} catch (error) {
+	console.error(error);
+}
+};
+async function comentarios(id){
+	const url = `https://youtube138.p.rapidapi.com/video/comments/?id=${id}&hl=en&gl=US`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c3a7a1fa4msh0e92c078fd61424p137867jsnebb20098d172',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+try {
+	let body2 = "";
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result.comments);
+	result.comments.forEach(e => {
+		body2 += `<div><img  class="icoComen" src="${e.author.avatar[0].url}">${e.author.title}<p>${e.content}</p></div>`;
+	});
+	return document.getElementById("comentarios").innerHTML = body2;
+
+} catch (error) {
+	console.error(error);
+}
+};
+async function videoRelacionados(id){
+	const url = `https://youtube138.p.rapidapi.com/video/related-contents/?id=${id}&hl=en&gl=US`;
+	const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c3a7a1fa4msh0e92c078fd61424p137867jsnebb20098d172',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+try {
+	let body3 = "";
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+	result.contents.forEach(e=>{
+		body3 += `<div><iframe width="100%" height="500" src="https://www.youtube.com/embed/${e.video.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>`
+	});
+	return document.getElementById("body").innerHTML = body3;
 } catch (error) {
 	console.error(error);
 }
