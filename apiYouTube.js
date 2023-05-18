46a2397ae4fdeec76cfbc851c8325d54f5a7d6c4
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-18 17:09:48 -0500

    feat: :art: agregue stylo
    se agrego un stylo a los videos relacionados de scroll

--- apiYouTube.js
+++ apiYouTube.js
@@ -1,4 +1,5 @@
 //autor:Wilfer Olrando Acosta Rodriguez
+
 async function buscar() {
     const buscar = document.getElementById("buscar").value;
     const url = `https://youtube138.p.rapidapi.com/search/?q=${buscar}&hl=en&gl=US`;
@@ -10,9 +11,13 @@ const options = {
 	}
 };
 try {
+	let body= "";
 	const response = await fetch(url, options);
 	const result = await response.json();
+  console.log(result);
+	console.log();
 	const id = result.contents[0].video.videoId;
+
     document.getElementById("video").innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	<h1><img src="${result.contents[0].video.author.avatar[0].url}" alt="avatar">${result.contents[0].video.title}<span>(${result.contents[0].video.publishedTimeText}) vistas:${result.contents[0].video.stats.views}</span></h1><br>
 	<h2>Descripcion</h2>
@@ -23,7 +28,7 @@ try {
 } catch (error) {
 	console.error(error);
 }
-};
+}
 async function comentarios(id){
 	const url = `https://youtube138.p.rapidapi.com/video/comments/?id=${id}&hl=en&gl=US`;
 const options = {
@@ -42,12 +47,13 @@ try {
 	result.comments.forEach(e => {
 		body2 += `<div><img  class="icoComen" src="${e.author.avatar[0].url}">${e.author.title}<p>${e.content}</p></div>`;
 	});
-	return document.getElementById("comentarios").innerHTML = body2;
+	document.getElementById("comentarios").innerHTML = body2;
 
 } catch (error) {
 	console.error(error);
 }
-};
+}
+
 async function videoRelacionados(id){
 	const url = `https://youtube138.p.rapidapi.com/video/related-contents/?id=${id}&hl=en&gl=US`;
 	const options = {
@@ -64,10 +70,12 @@ try {
 	const result = await response.json();
 	console.log(result);
 	result.contents.forEach(e=>{
-		body3 += `<div><iframe width="100%" height="500" src="https://www.youtube.com/embed/${e.video.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>`
+		console.log(e)
+		body3 += `<div><iframe width="280px" height="280" src="https://www.youtube.com/embed/${e.video.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>`
+		console.log(body3);
 	});
-	return document.getElementById("body").innerHTML = body3;
+	document.getElementById("body").innerHTML = body3;
 } catch (error) {
 	console.error(error);
 }
-};
+}
